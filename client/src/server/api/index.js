2c1cd1382faa16029527dc6d983bdcd584ebbfd1
sarahstandish <z.sisyphusrocks@gmail.com>
2021-08-27 14:19:18 -0700

    remove old build folder

--- client/src/server/api/index.js
+++ client/src/server/api/index.js
@@ -3,17 +3,17 @@ import axios from 'axios'
 let url;
 
 if (process.env.NODE_ENV === 'production') {
-    url = 'index.html'
+    url = 'https://flickr-search-with-dpi.herokuapp.com'
 } else {
-    url = 'http://localhost:8000/api'
+    url = 'http://localhost:8000'
 }
 
 const api = axios.create({
     baseURL: url
 })
 
-export const newCall = info => api.post('/searchCalls', info)
-export const getCalls = () => api.get('/searchCalls')
+export const newCall = info => api.post('/api/searchCalls', info)
+export const getCalls = () => api.get('/api/searchCalls')
 
 const apis = {
     newCall,
